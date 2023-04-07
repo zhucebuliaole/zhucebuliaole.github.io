de482e727bd11cccf92f5da41d9324be34d94409
Qile Liang <liangqile@outlook.com>
2023-04-07 21:35:33 +0800

    Claudia

---
+++ themes/Claudia/enviroment.sh
@@ -0,0 +1,17 @@
+#必要
+npm install hexo-renderer-pug 
+npm uninstall hexo-renderer-sass
+npm i --save hexo-renderer-sass-next
+npm install hexo-generator-search
+
+#选择性安装
+# 如果需要添加订阅功能
+npm install hexo-generator-feed
+# 流程图功能
+npm install hexo-filter-flowchart --save   
+# Emoji
+npm install hexo-filter-github-emojis --save  
+# 搜索功能
+npm install hexo-generator-search --save   
+# 数学公式
+npm install hexo-renderer-mathjax --save
